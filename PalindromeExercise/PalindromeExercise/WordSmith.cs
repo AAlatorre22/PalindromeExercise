be157d747a79a56508f7c60946d219052a9c9f04
Alejandro Alatorre <alejandro.c.alatorre@gmail.com>
2023-03-30 11:51:46 -0700

    Palindrome kayak complete

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+
+
+        public bool IsAPalindrome(string palin)
+        {
+            if ((palin == null) || (palin.Length == 0))
+            {
+                return false;
+            }
+
+            var reversedStr = new string(palin.ToCharArray().Reverse().ToArray());
+
+            if (reversedStr == palin)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+
+
+
+
+
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,21 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("kayak", true)]
+        [InlineData("madam", true)]
+        [InlineData("radar", true)]
+        [InlineData(null, false)]
+        [InlineData("", false)]
+        public void Test1(string input, bool expected)
         {
-
+            //Arrange
+            var palindrome = new WordSmith(); 
+            //Act
+            var actual = palindrome.IsAPalindrome(input);
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
